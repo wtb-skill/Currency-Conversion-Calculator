b69e074675069ba3a13a860e50955b0956c98525
Adam <adambaldyga@gmail.com>
2023-11-04 09:42:29 +0100

    Add get_rates() that gets exchange rates from NBP as a json, Add flask framework to calculate the exchange rates for chosen currency and set money value, Add calculator.html to display the calculator

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 import requests
 from flask import Flask, render_template, request
+import locale
 
 
 def get_rates():
     response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
     response.raise_for_status()
-    data = response.json()
-    return data
+    _data = response.json()
+    return _data
 
+
+data = get_rates()
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
-def book_manager():
-    # if request.method == "POST":
-    #     data = request.form
-    #     title = data.get('title')
-    #     author = data.get("author")
-    #     release_date = data.get('release_date')
-    #     comment = data.get("comment")
+def currency_calculator():
+    result = None
+    if request.method == "POST":
+        selected_currency = request.form['currency']
+        amount = float(request.form['amount'])
+        for rate in data[0]['rates']:
+            if rate['code'] == selected_currency:
+                exchange_rate = rate['ask']
+                result = amount * exchange_rate
+                break  # Stop searching when you find the selected currency
 
+        formatted_result = locale.currency(result, grouping=True)
 
+        return render_template('calculator.html', data=data, result=formatted_result,
+                               selected_currency=selected_currency)
 
-    return render_template("calculator.html")
+    return render_template("calculator.html", data=data)
 
 
 if __name__ == '__main__':
+    locale.setlocale(locale.LC_ALL, 'pl_PL.utf8')  # Polish locale
     app.run(debug=True)
+    # print(data)
 
